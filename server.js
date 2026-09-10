023f63fd5abbf3596357f6efdc0e570f2738dd9c
Gustavo <gustavooterrivel2017@gmail.com>
2026-09-10 09:32:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = require('./src');
 
 const PORT = 3000;
 
-app.get("/", (req,res)=>{
-    res.send("API funcionando!");
-});
-
 app.listen(PORT, ()=> {
     console.log(`Servidor iniciado na porta ${PORT}`)
 })
